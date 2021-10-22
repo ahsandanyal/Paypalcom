11f30bf8693b18b2b3e435812f2af241bfd327c6
ahsandanyal <Ahsandanyal97@gmail.com>
2021-10-22 22:22:31 +0500

    update clone paypal

--- script.js
+++ script.js
@@ -24,15 +24,49 @@ document.querySelectorAll('.signup').forEach(signupBtn => {
 
 
 const dropdownItems = document.querySelectorAll('.nav-dropdown');
-
-dropdownItems.forEach(dropdownItem =>{
-    dropdownItem.addEventListener('mouseover', ()=>{
-        dropdownItem.lastElementChild.style.cssText = 'opacity:1; visibility:visible'
-        document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+if(window.innerWidth < 1000){
+    const menuIcon = document.querySelector('.menu')
+    const navbar = document.querySelector('.navbar')
+    menuIcon.addEventListener('click', ()=> {
+        navbar.classList.toggle('change')
+        // if(!navbar.classList.contains('change')){
+        //     document.querySelectorAll('.nav-dropdown').forEach(dropdown =>{
+        //         dropdown.style.left ='-20rem'
+        //     })
+        // }
+    })
+    document.querySelectorAll('.show-dropdown').forEach
+    (link =>{
+        link.addEventListener('click', ()=>{
+            link.nextElementSibling.style.left = '0'
+        })
+    })
+    document.querySelectorAll('.dropdown-heading-link').forEach(headinglink => {
+        headinglink.addEventListener('click', ()=>{
+            headinglink.parentElement.parentElement.style.left = '-20rem'
+        })
+    })
+}else{
+    dropdownItems.forEach(dropdownItem => {
+        dropdownItem.addEventListener('mouseover', ()=>{
+            dropdownItem.lastElementChild.style.cssText = 'opacity:1; visibility:visible'
+            document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+            dropdownItem.firstElementChild.firstElementChild
+            .style.transform = 'rotate(180deg)'
+        })
+    
+        dropdownItem.addEventListener('mouseover', ()=>{
+            dropdownItem.lastElementChild.style.cssText = 'opacity:0; visibility:hidden'
+            document.querySelector('.navbar-wrapper').style.background = 'none'
+            dropdownItem.firstElementChild.firstElementChild
+            .style.transform = 'rotate(0)'
+    })
+    })
+    window.addEventListener('resize', ()=>{
+        window.location.reload()
     })
 
-    dropdownItem.addEventListener('mouseover', ()=>{
-        dropdownItem.lastElementChild.style.cssText = 'opacity:0; visibility:hidden'
-        document.querySelector('.navbar-wrapper').style.background = 'none'
-})
-})
+}
+
+
+
